568d8299f741766a1fde3b0f98187bc5604f8263
Mónica Domingues <monicamd2004@gmail.com>
2025-04-27 20:03:11 +0100

    feat(styles): add global colors variables and update button styles
    - Defined the colors' variables for consistent primary color usage.
    - Ensured the button styles are globally applicable.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-import styles from './Header.module.css';
+import header_styles from './Header.module.css';
 import logo from '../../assets/images/a3_logo_horizontal.svg';
 import search_icon from '../../assets/images/search_icon.svg';
+import shopping_cart_icon from '../../assets/images/shopping_cart_icon.svg';
 import NavBarGroup from './NavBarGroup.jsx'
-import { useLocation, useNavigate  } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 
@@ -11,7 +12,7 @@ export default function Header() {
   const navigate = useNavigate()
 
   return (
-    <header className={`${styles.header}`}>
+    <header className={`${header_styles.header}`}>
       <div className='row align-items-center'>
         <div className='col-auto'>
           <a href="">
@@ -22,57 +23,67 @@ export default function Header() {
         <div className='col-4'>
           <div className="input-group">
             <input
-              className={`form-control form-control-md ${styles.inputField}`}
+              className={`form-control form-control-md ${header_styles.inputField}`}
               type="text"
               placeholder="Pesquisar produtos..."
             />
-            <span 
-            className={`input-group-text ${styles.inputField}`}>
-              <img src={search_icon} alt=""/>
+            <span className={`input-group-text ${header_styles.inputField}`}>
+              <img src={search_icon} alt="" />
             </span>
           </div>
         </div>
+
+        <div className='col-auto ms-auto d-flex align-items-center gap-3'>
+          <button className={`col-auto icon-button`}>
+            <img src={shopping_cart_icon} alt="Ícone de carrinho de compras" />
+          </button>
+
+          <button className={`col-auto primary-button`}>Login</button>
+
+        </div>
       </div>
 
-    <div className='d-flex justify-content-start gap-5 ps-3 pt-2 pb-4'>
-      <button 
-        className={`col-auto ${styles.headerButton} ${location === '/' ? `${styles.selectedButton}` : ''}`}
-        onClick={() => navigate('/')}
-      >
-        
+
+
+      <div className='d-flex justify-content-start gap-5 ps-3 pt-2 pb-4'>
+        <button
+          className={`col-auto ${header_styles.headerButton} ${location === '/' ? `${header_styles.selectedButton}` : ''}`}
+          onClick={() => navigate('/')}
+        >
+
           Página Inicial
         </button>
 
-        <div className={`col-auto ${styles.hoverWrapper}`}>
-          <button className={`${styles.headerButton} ${styles.headerButtonProdutos}`}>Produtos</button>
-          <div className={styles.hoverDialog}>
-            <div className={styles.hoverDialogOverhead}></div>
-            <div className={styles.hoverDialogContent}>
+        <div className={`col-auto ${header_styles.hoverWrapper}`}>
+          <button className={`${header_styles.headerButton} ${header_styles.headerButtonProdutos}`}>Produtos</button>
+          <div className={header_styles.hoverDialog}>
+            <div className={header_styles.hoverDialogOverhead}></div>
+            <div className={header_styles.hoverDialogContent}>
               <div className='row'>
                 <NavBarGroup
                   title="Animais"
                   items={[
-                    {label: "Aves", href: ""},
-                    {label: "Domésticos", href: ""},
-                    {label: "Mamíferos Selvagens", href: ""},
-                    {label: "Insetos e Aracnídeos", href: ""},
-                    {label: "Répteis", href: ""},
+                    { label: "Aves", href: "" },
+                    { label: "Domésticos", href: "" },
+                    { label: "Mamíferos Selvagens", href: "" },
+                    { label: "Insetos e Aracnídeos", href: "" },
+                    { label: "Répteis", href: "" },
                   ]}
                 />
 
                 <NavBarGroup
                   title="Material de Impressão"
                   items={[
-                    {label: "Acessórios", href: ""},
-                    {label: "Filamentos", href: ""},
-                    {label: "Impressoras", href: ""},
+                    { label: "Acessórios", href: "" },
+                    { label: "Filamentos", href: "" },
+                    { label: "Impressoras", href: "" },
                   ]}
                 />
 
                 <NavBarGroup
                   title="Pedido Personalizado"
                   items={[
-                    {label: "Fazer um pedido personalizado", href: ""},
+                    { label: "Fazer um pedido personalizado", href: "" },
                   ]}
                 />
 
@@ -81,7 +92,7 @@ export default function Header() {
           </div>
         </div>
 
-        <button className={`col-auto ${styles.headerButton}`}>Sobre Nós</button>
+        <button className={`col-auto ${header_styles.headerButton}`}>Sobre Nós</button>
       </div>
 
     </header>

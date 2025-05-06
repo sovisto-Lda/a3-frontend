e3a942a04cd4d93f0beb2bf8e3fcb501b2dfb461
Pedro Grilo <103907484+PedroG277@users.noreply.github.com>
2025-05-06 18:25:28 +0100

    feat: home page with top and custom order cta components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
+import FeaturedCard from '../components/Product_Cards/featured_card.jsx';
+import { useEffect, useRef, useState } from 'react';
+import Home_Top from '../components/Home_Sections/Home_Top.jsx';
+import Custom_Order_CTA from '../components/misc/Custom_Order_CTA/Custom_Order_CTA.jsx';
+
 export default function Home() {
+
   return (
-    <main>
-      <h1>A tua prima a voar.</h1>
-      <a href="/users">Ver users</a>
+    <main >
+
+      <Home_Top/>
+
+      <Custom_Order_CTA/>
+
     </main>
   );
 }

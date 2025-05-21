6db72b3f4ebe345bd7a13055647a4d1fe56959e4
Pedro Grilo <103907484+PedroG277@users.noreply.github.com>
2025-05-21 12:08:26 +0100

    refactor: rename Header_Top to Hero across project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import FeaturedCard from '../components/Product_Cards/featured_card.jsx';
 import { useEffect, useRef, useState } from 'react';
-import Home_Top from '../components/Home_Sections/Home_Top.jsx';
+import Hero from '../components/Home_Sections/Hero/Hero.jsx';
 import Custom_Order_CTA from '../components/misc/Custom_Order_CTA/Custom_Order_CTA.jsx';
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main >
 
-      <Home_Top/>
+      <Hero/>
 
       <Custom_Order_CTA/>
 

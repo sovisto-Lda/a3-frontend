302880d00ed90ea2bea77af52609cef0eaf98dca
softnotsoft <guipereiramartins.gm@gmail.com>
2025-06-12 17:41:46 +0100

    style: improve layout alignment and element sizing

--- src/components/Products/Category_Card.jsx
+++ src/components/Products/Category_Card.jsx
@@ -1,14 +1,18 @@
 
 
-export default function Category_Card({ src, title }) {
+export default function Category_Card({ image, name }) {
     return (
-        <div className="col">
-            <img 
-                src={src}
-                className="img-fluid" 
-                alt={`Imagem da categoria ${title}`}>
-            </img>
-            <h4>{title}</h4>
+        <div className="d-flex flex-column w-100">
+            <div style={{ aspectRatio: '1.15', overflow: 'hidden', height: "200px"}}>
+                <img 
+                src={image}
+                className="img-fluid rounded-1 w-100 h-100 object-fit-cover" 
+                alt={name}>
+                </img>
+            </div>
+            <div className = "d-flex flex-column mt-1" style={{ flexShrink: "1", minWidth: "0"}}>
+                <h6 className="fw-bold mb-1" style={{whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden", fontSize: "24px"}}>{name}</h6>
+            </div>
         </div>
     )
 }
--- src/components/misc/Custom_Order_CTA/Custom_Order_CTA.jsx
+++ src/components/misc/Custom_Order_CTA/Custom_Order_CTA.jsx
@@ -1,8 +1,8 @@
 import styles from './Custom_Order_CTA.module.css'
 
-export default function Custom_Order_CTA() {
+export default function Custom_Order_CTA({ noMargin = false }) {
     return (
-        <div className={`d-flex flex-column flex-sm-row justify-content-center align-items-center gap-3 ${styles.wrapper}`}
+        <div className={`d-flex flex-column flex-sm-row justify-content-center align-items-center gap-3 ${styles.wrapper} ${noMargin ? styles.noMargin : ''}`}
         >
             <h1 className='m-0'>Não encontra o que procura?</h1>
     

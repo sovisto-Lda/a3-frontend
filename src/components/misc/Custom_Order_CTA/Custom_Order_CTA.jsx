e730a79aad4bcd11fd9233912c4a4fc77838fab6
Pedro Grilo <103907484+PedroG277@users.noreply.github.com>
2025-06-13 01:26:27 +0100

    feat: create coming soon page and route applicable buttons and anchors

--- src/components/misc/Custom_Order_CTA/Custom_Order_CTA.jsx
+++ src/components/misc/Custom_Order_CTA/Custom_Order_CTA.jsx
@@ -1,12 +1,18 @@
 import styles from './Custom_Order_CTA.module.css'
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Custom_Order_CTA() {
+    const navigate = useNavigate();
+
     return (
         <div className={`d-flex flex-column flex-sm-row justify-content-center align-items-center gap-3 ${styles.wrapper}`}
         >
             <h1 className='m-0'>Não encontra o que procura?</h1>
     
-            <div className='primary-button'><h1>Personalize o seu produto</h1></div>
+            <button className='primary-button' onClick={()=> {navigate('/comingsoon')}}>
+                <h1>Personalize o seu produto</h1>
+            </button>
         </div>
     )
 

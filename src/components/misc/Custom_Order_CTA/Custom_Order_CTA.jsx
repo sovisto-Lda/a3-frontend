0c1db3d851d986d0c729a6c7bb0f681abc651539
Pedro Grilo <103907484+PedroG277@users.noreply.github.com>
2025-06-13 17:44:46 +0100

    fix: custom order cta responsiveness

--- src/components/misc/Custom_Order_CTA/Custom_Order_CTA.jsx
+++ src/components/misc/Custom_Order_CTA/Custom_Order_CTA.jsx
@@ -3,16 +3,16 @@ import { useNavigate } from 'react-router-dom';
 
 
 
-export default function Custom_Order_CTA({ noMargin = false }) {
+export default function Custom_Order_CTA({ noMargin = false,  bg=false}) {
     const navigate = useNavigate()
 
     return (
-        <div className={`d-flex flex-column flex-sm-row justify-content-center align-items-center gap-3 ${styles.wrapper} ${noMargin ? styles.noMargin : ''}`}
+        <div className={`d-flex flex-column flex-sm-row justify-content-center align-items-center gap-3 ${styles.wrapper} ${noMargin ? styles.noMargin : ''} ${bg ? styles.bg : ''}`}
         >
-            <h1 className='m-0'>Não encontra o que procura?</h1>
+            <h1 className={`m-0 ${styles.text}`}>Não encontra o que procura?</h1>
     
-            <button className='primary-button' onClick={()=> {navigate('/comingsoon')}}>
-                <h1>Personalize o seu produto</h1>
+            <button className='primary-button' onClick={()=> {navigate('/comingsoon')}} style={{textAlign: 'start'}}>
+                <h1 className={styles.text}>Personalize o seu produto</h1>
             </button>
         </div>
     )

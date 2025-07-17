788d70f9c43837c8c0dc7fd2a380123870cfdb4e
PedroG277 <pargo@iscte-iul.pt>
2025-07-17 15:54:29 +0000

    feat: remove console logs on production build

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
+import removeConsole from 'vite-plugin-remove-console';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
@@ -7,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [react(), removeConsole()],
     root: path.resolve(__dirname, 'src'),
     publicDir: path.resolve(__dirname, 'src/assets'),
     build: {
